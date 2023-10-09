3a589b30c0805127cbe04bccc3e7732e12015a9e
Niraj Shrestha <nshrest23@Nirajs-MacBook-Air.local>
2023-10-09 12:33:14 +0545

    day5 - ORM database models stuffs and updated few missing templates

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from orders.models import Cart
+from orders.models import Cart, Order, OrderSummary
 
 # Register your models here.
 admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderSummary)
--- orders/models.py
+++ orders/models.py
@@ -12,6 +12,23 @@ class Cart(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    delivery_status = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+    total = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)  
+
+class OrderSummary(models.Model):
+    subtotal = Order.total
+    reciept_voucher = models.CharField()
+    invoice_number = models.IntegerField()
+    invoice_date = models.DateField(auto_now_add=True)
+    discount = models.FloatField(default=0)
+    tax = models.FloatField(default=0)
+    delivery_charge = models.FloatField(default=0)
+    total_amount = models.FloatField(default=0)
     
 
 def __str__(self):
