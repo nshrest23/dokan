3a6ce69ee3bf78056f3da404004fcfbaff26bdd4
Niraj Shrestha <nshrest23@Nirajs-MacBook-Air.local>
2023-10-18 08:59:26 +0545

    day12 - working purchase page & fixed update_cart functionality

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("checkout", views.checkout, name="checkout_page"),
     path("order", views.order, name="order_page"),
     path("addcart/<int:product_id>", views.add_cart, name="addcart_page"),
+    path("updatecart/<int:cart_id>", views.update_cart, name="updatecart_page"),
     path("deletecart/<int:cart_id>", views.delete_cart, name="deletecart_page"),
     path("purchase", views.purchase, name="purchase_page"),
     path("thanks", views.thanks, name="thanks_page"),
